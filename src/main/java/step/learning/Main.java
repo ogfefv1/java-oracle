0a1b23009a6101a83580777fae8bfcbb3ffbf160
dzhem <itstepstudentdzhem06@gmail.com>
2026-09-08 12:56:01 +0300

    .

--- src/main/java/step/learning/Main.java
+++ src/main/java/step/learning/Main.java
@@ -1,11 +1,12 @@
 package step.learning;
 
 import step.learning.AsyncDemo.AsyncDemo;
+import step.learning.AsyncDemo.TaskDemo;
 import step.learning.OOPDemo.OOPDemo;
 
 
 public class Main {
     static void main(String[] args) {
-        new AsyncDemo().run();
+        new TaskDemo().run();
     }
 }
