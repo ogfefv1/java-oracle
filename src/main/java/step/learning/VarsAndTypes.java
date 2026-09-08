30ff259d88541ccb9ce8ab8961e17894c964ecde
dzhem <itstepstudentdzhem06@gmail.com>
2026-09-08 10:18:24 +0300

    Initial commit

---
+++ src/main/java/step/learning/VarsAndTypes.java
@@ -0,0 +1,23 @@
+package step.learning;
+
+public class VarsAndTypes {
+    public void run() {
+        byte b = -120;
+        short s = 12000;
+        int i = 1200000;
+        long l = 12000000000L;
+        float f = 3.14f;
+        double d = 2.718281828459045;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
