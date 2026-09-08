ef200e59948d875d0316196f82e971bd1f546716
dzhem <itstepstudentdzhem06@gmail.com>
2026-09-08 11:24:55 +0300

    .

---
+++ src/main/java/step/learning/OOPDemo/Literature.java
@@ -0,0 +1,15 @@
+package step.learning.OOPDemo;
+
+public abstract class Literature {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public abstract String card();
+}
