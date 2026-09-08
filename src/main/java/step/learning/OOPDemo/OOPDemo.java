dbff8841047fba9467752d62afe63b453c8d2523
dzhem <itstepstudentdzhem06@gmail.com>
2026-09-08 11:47:41 +0300

    .

--- src/main/java/step/learning/OOPDemo/OOPDemo.java
+++ src/main/java/step/learning/OOPDemo/OOPDemo.java
@@ -1,5 +1,6 @@
 package step.learning.OOPDemo;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,17 @@ public class OOPDemo {
         library.add(new Book("How to Talk to Your Cat About Taxes", "P. Whiskers"));
         library.add(new Booklet("Safety Rules at the Workshop", "Step IT Academy"));
         library.add(new Booklet("How to Assemble a Wardrobe", "IKEA Press"));
+        library.add(new Journal("Science Today", 42));
+        try {
+            library.add(
+                    new Newspaper("The Daily Compiler",
+                            Newspaper.dateFormat.parse("04.09.2026"))
+            );
+        }
+        catch (ParseException ex) {
+            System.getLogger(OOPDemo.class.getName())
+                    .log(System.Logger.Level.ERROR, ex.getMessage());
+        }
         for (Literature lit : library) {
             System.out.println( lit.card() );
         }
